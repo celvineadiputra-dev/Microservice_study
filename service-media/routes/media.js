e0c3b9b35795c15b145487d9f5addfcce46b9ff8
celvineadiputra-dev <celvineadiputra2@gmail.com>
2021-02-11 10:19:51 +0700

    upload image service-media

--- service-media/routes/media.js
+++ service-media/routes/media.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const isBase64 = require('is-base64');
+const base64Img = require('base64-img');
+const { Media } = require('../models');
 
-/* GET media listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', (req,res)=>{
+  const image = req.body.image;
+  if(!isBase64(image, {mimeRequired: true})){
+    return res.status(400).json({
+      status: 'error', 
+      message: "invalid base64"
+    });
+  }
+  base64Img.img(image, './public/images/', Date.now(),async (err, filepath)=>{
+    if(err){
+      return res.status(400).json({
+        status : 'error',
+        message : err.message
+      });
+    }
+
+    const fileName = filepath.split("\\").pop().split("/").pop();
+    const media = await Media.create({ image: `image/${fileName}` });
+
+    return res.status(200).json({
+      status : "success",
+      data : {
+        id : media.id,
+        image : `${req.get('host')}/images/${fileName}`
+      }
+    });
+
+  });
 });
 
 module.exports = router;
