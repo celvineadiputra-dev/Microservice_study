53102789a00cff0bd860c6de8085896eb63a8803
Celvine <celvineadiputra2@gmail.com>
2021-02-17 09:26:28 +0700

    membuat model fresh token

---
+++ service-user/models/refreshToken.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, DataTypes)=>{
+    const refreshToken = Sequelize.define('refreshToken',{
+        id : {
+            type : DataTypes.INTEGER,
+            autoIncrement : true,
+            primaryKey : true,
+            allowNull : false
+        },
+        token : {
+            type : DataTypes.TEXT,
+            allowNull : false
+        },
+        user_id : {
+            type : DataTypes.INTEGER,
+            allowNull : false
+        },
+        created_at : {
+            type : DataTypes.DATE,
+            field : 'created_at',
+            allowNull : false
+        },
+        updated_at : {
+            type : DataTypes.DATE,
+            field: 'updated_at',
+            allowNull : false
+        }
+    },{
+        tableNameeeee: 'refresh_tokens',
+        timestamps : true
+    });
+    return refreshToken;
+}
