cf00828e76f13f9ea7b05e04e7ac8b02c48d7328
Celvine <celvineadiputra2@gmail.com>
2021-02-19 19:10:42 +0700

    Add update user profile

--- service-user/routes/users.js
+++ service-user/routes/users.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const usersHandler = require('./handler/users');
 
-router.post('/register',usersHandler.register);
+router.post('/register', usersHandler.register);
 router.post('/login', usersHandler.login);
+router.put('/:id', usersHandler.update);
 
 module.exports = router;
