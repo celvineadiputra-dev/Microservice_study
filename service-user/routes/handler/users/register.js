82c919dd9b998d70f17737a2967828f01e325386
Celvine <celvineadiputra2@gmail.com>
2021-02-19 19:11:00 +0700

    beauty line code

--- service-user/routes/handler/users/register.js
+++ service-user/routes/handler/users/register.js
@@ -1,46 +1,48 @@
-const { User } = require('../../../models');
+const {
+    User
+} = require('../../../models');
 const bcrypt = require('bcrypt');
 const Validator = require('fastest-validator')
 const v = new Validator();
 
-module.exports = async (req, res) =>{
+module.exports = async (req, res) => {
     const schema = {
-        name : 'string|empty:false',
-        email : 'string|empty:false',
-        password : 'string|min:6',
-        profession : 'string|optional'
+        name: 'string|empty:false',
+        email: 'string|empty:false',
+        password: 'string|min:6',
+        profession: 'string|optional'
     }
     const validate = v.validate(req.body, schema);
 
-    if(validate.length){
+    if (validate.length) {
         return res.status(400).json({
-            status : 'error',
-            message : validate
+            status: 'error',
+            message: validate
         });
     }
     const userData = await User.findOne({
-        where : {
-            email : req.body.email
+        where: {
+            email: req.body.email
         }
     });
-    if (userData){
+    if (userData) {
         return res.status(409).json({
-            status : 'error',
-            message : "email already exist"
+            status: 'error',
+            message: "email already exist"
         })
     }
     const password = await bcrypt.hash(req.body.password, 10);
     const data = {
         password,
-        name : req.body.name,
-        email : req.body.email,
+        name: req.body.name,
+        email: req.body.email,
         profession: req.body.profession,
-        role : 'student'
+        role: 'student'
     }
     const createUser = await User.create(data);
     return res.status(200).json({
-        status : 'success',
-        data : {
+        status: 'success',
+        data: {
             id: createUser.id
         }
     })
