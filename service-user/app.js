4a53bebe36420b5bea995c6f1975c86ac1378204
Celvine <celvineadiputra2@gmail.com>
2021-02-22 09:39:00 +0700

    init router refreshtoken

--- service-user/app.js
+++ service-user/app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const refreshTokenRouter = require('./routes/refreshToken')
 
 const app = express();
 
@@ -19,5 +20,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.user('/refreshtoken', refreshTokenRouter);
 
 module.exports = app;
