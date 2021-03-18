5ee6f3652025eb2ca64d2ff1c5cefbb911a9e053
CelvineAdiPutra <celvineadiputra2@gmail.com>
2021-03-18 09:45:39 +0700

    Integrasi API Update

--- api-gateway/routes/services/users.js
+++ api-gateway/routes/services/users.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const userHandler = require('../handler/users')
+const verifyToken = require('../../middlewares/verifyToken');
 
 router.post('/register', userHandler.register);
 router.post('/login', userHandler.login);
+router.put('/', verifyToken ,userHandler.update);
 
 module.exports = router;
